a9ebcd31669fe4f36f3f734bee44094abe140e75
Vakhtanh Chikhladze <90832378+vc-cu@users.noreply.github.com>
2022-03-22 21:00:18 +0200

    updated mock token

--- contracts/Mocks/MyToken.sol
+++ contracts/Mocks/MyToken.sol
@@ -5,12 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
 
-    constructor(string memory name_, string memory symbol_) public ERC20(name_, symbol_) {
+    uint8 private _decimals;
 
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) public ERC20(name_, symbol_) {
+        _decimals = decimals_;
     }
 
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
+    }
+
+    function decimals() public view override returns (uint8){
+        return _decimals;
     }
 
 }
